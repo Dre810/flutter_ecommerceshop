21259657e6228946abf59d1baf07a5080f796da6
Davis Kamau <daviskamau810@gmail.com>
2025-12-02 20:59:58 +0300

    added app_colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:starter_project/constants/app_colors.dart';
 import './screens/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+      theme: ThemeData(scaffoldBackgroundColor: AppColors.lightScaffoldColor),
 
       home: HomeScreen(),
     ),
